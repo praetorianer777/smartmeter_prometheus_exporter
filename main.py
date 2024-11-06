d85782d87d5a7f5359d7d8984c0cd054d31c12ef
Steve Berthold <praetorianer777@mail.de>
2024-11-06 13:42:45 +0100

    loop over all obis values and filter depending on the specific three

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ CURRENT_ENERGY = Gauge('current_energy_watts', 'This is the current energy drawn
 TOTAL_ENERGY = Gauge("total_energy_watthours", "This is the total energy received from the grid", ["name", "metric", "unit"])
 TOTAL_ENERGY_FEED = Gauge("total_energy_feed_watthours", "This is the total energy fed to the grid", ["name", "metric", "unit"])
 
+# 0100020800ff total feed to grid
+# 0100100700ff current energy
+# 0100010800ff total energy from grid
+
+
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(port)
@@ -35,9 +40,13 @@ if __name__ == '__main__':
             sml_frame = stream.get_frame()
             if sml_frame is not None:
                 obis_values = sml_frame.get_obis()
-                TOTAL_ENERGY.labels(name=device, metric="total_energy", unit="Wh").set(obis_values[2].value)
-                TOTAL_ENERGY_FEED.labels(name=device, metric="total_energy", unit="Wh").set(obis_values[3].value)
-                CURRENT_ENERGY.labels(name=device, metric="current_energy", unit="W").set(obis_values[4].value)
+                for obis in obis_values:
+                    if obis.obis == "0100010800ff":
+                        TOTAL_ENERGY.labels(name=device, metric="total_energy", unit="Wh").set(obis_values[2].value)
+                    if obis.obis == "0100020800ff":
+                        TOTAL_ENERGY_FEED.labels(name=device, metric="total_energy", unit="Wh").set(obis_values[3].value)
+                    if obis.obis == "0100100700ff":
+                        CURRENT_ENERGY.labels(name=device, metric="current_energy", unit="W").set(obis_values[4].value)
 
     except KeyboardInterrupt:
         print("Stopping...")
