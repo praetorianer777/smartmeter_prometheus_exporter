956c16dc510fd2e030004a4171fc3b38e2a4e68d
Steve Berthold <praetorianer777@mail.de>
2024-11-06 12:26:21 +0100

    init

---
+++ main.py
@@ -0,0 +1,42 @@
+from smllib import SmlStreamReader
+import serial
+from prometheus_client import start_http_server, Summary, Gauge, Info, Counter
+
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+
+stream = SmlStreamReader()
+ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
+INFO = Info('Smartmeter_Prometheus_Adapter', 'This adapter gives energy metrics about the heat pump')
+CURRENT_ENERGY = Gauge('heat_pump_current_energy_watts', 'This is the current energy drawn from the grid', ["name", "metric", "unit"])
+TOTAL_ENERGY = Gauge("heat_pump_total_energy_watthours", "This is the total energy received from the grid", ["name", "metric", "unit"])
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    try:
+        while True:
+            # Read bytes from the serial port
+            data = ser.read(32)  # Read up to 32 bytes at a time
+            stream.add(data)
+            sml_frame = stream.get_frame()
+            if sml_frame is None:
+                print('Bytes missing')
+            else:
+                obis_values = sml_frame.get_obis()
+                TOTAL_ENERGY.labels(name="heat_pump", metric="total_energy", unit="Wh").set(obis_values[2].value)
+                CURRENT_ENERGY.labels(name="heat_pump", metric="current_energy", unit="W").set(obis_values[4].value)
+                print(obis_values[2].value) # total energy from grid (Wh)
+                print(obis_values[4].value) # current energy from grid (W)
+                #parsed_msgs = sml_frame.parse_frame()
+                #for msg in parsed_msgs:
+                    #print(msg.format_msg())
+
+
+    except KeyboardInterrupt:
+        print("Stopping...")
+    finally:
+        # Close the serial port
+        ser.close()
+        print("Serial port closed.")
